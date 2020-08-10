3a3a89ad69061040418df2d90a1ff4819f408d16
masudabegum <masudab020@gmail.com>
2020-08-10 21:47:02 +0600

    car page added

--- assets/js/car.js
+++ assets/js/car.js
@@ -1,9 +1,39 @@
-let params = new URLSearchParams(location.search);
-console.log(params.get('carId'))
+let params = new URLSearchParams(window.location.search);
+let carId = params.get("carId");
 
-let carId = params.get('carId');
+console.log(carId);
+firebase
+  .firestore()
+  .collection("all-cars")
+  .doc(carId)
+  .get()
+  .then((snap) => {
+    let car = snap.data();
+    document.querySelector("#car-image img").src = car.image;
 
-firebase.firestore().collection("all-cars").doc(carId).get().then(snapshot=>{
-    let car=snapshot.data()
-    console.log(car)
-})
+    let details = `
+            <p class="lead"><b>Name: </b> ${car.name}</p>
+            <p class="lead"><b>Description: </b> ${car.description}</p>
+            <p class="lead"><b>Air Condition: </b> ${car.air_condition}</p>
+            <p class="lead"><b>Size: </b> ${car.size}</p>
+            <p class="lead"><b>Seats: </b> ${car.numberOfSits}</p>
+            <p class="lead"><b>One-way Fare: </b> ${car.one_way_fare}</p>
+            <p class="lead"><b>Two-way Fare: </b> ${car.two_way_fare}</p>
+            <p class="lead"><b>Starting Point: </b> ${car.from}</p>
+            <p class="lead"><b>Destinations: </b> ${car.locations}</p>
+            <p class="lead"><b>Driver: </b> ${car.driver}</p>
+            <p class="lead"><b>Driver Experience: </b> ${car.experience}</p>
+
+        `;
+
+    document.querySelector("#car-details").innerHTML = details;
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoibWlocmFiIiwiYSI6ImNrYXhxNWt1NTA4ZnMyc285ZnQ4eTJiZG4ifQ.2sYKfCuUIILbf04RJdnJDw";
+    var map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [90.3563, 23.6850],
+      zoom: 15
+    });
+  });
